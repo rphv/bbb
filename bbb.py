36b7fc48f0a27cd4f791810ef1fe19e0ace209d2
Perry Hooker <perry.hooker@gmail.com>
2023-06-26 09:33:14 -0600

    fix indentation, use constant for retries

--- bbb.py
+++ bbb.py
@@ -11,6 +11,7 @@ STATION = 'alpine'
 TARGET = 2
 POLL_INTERVAL = 600 # seconds
 URL = 'https://api.bridgerbowl.com/graphql'
+MAX_RETRIES = 6
 QUERY = """
 query Query($station: String!, $date_start: DateTime!, $date_end: DateTime!) {
   weather_readings(
@@ -64,59 +65,61 @@ def check_weather_at_bridger_bowl():
     logger.info("Start time: " + start_time)
     logger.info("End time:   " + end_time)
 
-    max_attempts = 6
-    for attempt in range(max_attempts):
-        logger.info("Fetching weather data, attempt " + str(attempt + 1) + " of " + str(max_attempts))
+    for attempt in range(MAX_RETRIES):
+        logger.info("Fetching weather data, attempt " + str(attempt + 1) + " of " + str(MAX_RETRIES))
         try:
             response = requests.post(URL, json=payload)
             response.raise_for_status()  # Raises error for unsuccessful status (i.e., not 2xx).
             weather_data = json.loads(response.text)
             logger.info("Latest weather data: %s", weather_data)
             return weather_data['data']['weather_readings']['data'][0][WEATHER_ATTRIBUTE] > TARGET
-        except Exception as e:
-            logger.error("Failed to fetch new weather data -- %s", e)
-            if attempt < max_attempts - 1:  # i.e. if it's not the final attempt
+        except (requests.exceptions.RequestException, json.JSONDecodeError) as e:
+            logger.error("Failed to fetch new weather data. Exception: %s", e)
+            if attempt < MAX_RETRIES - 1:  # i.e. if it's not the final attempt
                 time.sleep(2**attempt)  # exponential backoff
+            else:
+                raise # raise the last exception
 
-### Entry point ###
 
-# Set up logging
-logger = logging.getLogger("Rotating Log")
-logger.setLevel(logging.DEBUG)
+### Main ###
+try:
+    # Set up logging
+    logger = logging.getLogger("Rotating Log")
+    logger.setLevel(logging.DEBUG)
 
-# add a rotating handler
-handler = RotatingFileHandler("/home/bbb/bbb.log", maxBytes=1000000, backupCount=5)
-logger.addHandler(handler)
+    # add a rotating handler
+    handler = RotatingFileHandler("/home/bbb/bbb.log", maxBytes=1000000, backupCount=5)
+    logger.addHandler(handler)
 
-# Find first BlinkStick
-led = blinkstick.find_first()
+    # Find first BlinkStick
+    led = blinkstick.find_first()
 
-# Can't do anything if BlinkStick is not connected
-if led is None:
-    logging.error("BlinkStick not found...\r\nExiting...")
-else:
-    try:
+    # Can't do anything if BlinkStick is not connected
+    if led is None:
+        logging.error("BlinkStick not found. Exiting.")
+    else:
         # Store value of last state in this variable
-        is_new_snow = False
-        
+        is_weather_condition_met = False
         while (True):
             if check_weather_at_bridger_bowl():
                 # If previously there was no new snow, then log message
                 # and set BlinkStick to blinking blue
-                if not is_new_snow:
-                    logger.info("New snow detected!")
-                    is_new_snow = True
+                if not is_weather_condition_met:
+                    logger.info("Weather condition met!")
+                    is_weather_condition_met = True
                 # Each pulse is approx. 2s so this blocks for about 10 min
                 led.pulse(name="blue", repeats=POLL_INTERVAL/2)
             else:
                 # If snow found previously, then log message
-                if is_new_snow:
-                    logger.info("It stopped snowing...")
-                    is_new_snow = False
-                led.set_color(name="yellow")
+                if is_weather_condition_met:
+                    logger.info("Weather condition no longer met.")
+                    is_weather_condition_met = False
+                led.set_color(name="white")
                 time.sleep(POLL_INTERVAL)
-            logger.info("--------------------------------------")
-    except KeyboardInterrupt:
-        logger.info("Exiting... Bye!")
-
+                logger.info("--------------------------------------")
+except KeyboardInterrupt:
+    logger.info("Interrupted by user. Exiting... Bye!")
     led.turn_off()
+except Exception as e:
+    logger.error("Exiting with exception: %s", e)
+    led.set_color(name="red")
