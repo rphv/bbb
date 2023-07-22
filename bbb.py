5287ca776062f00c9644fa268058ba35b370cc07
rphv <perry.hooker@gmail.com>
2023-07-21 19:56:58 -0600

    update for Charlieplex LED bonnet
    implements falling pixels pattern and
    increases max retries

--- bbb.py
+++ bbb.py
@@ -1,18 +1,34 @@
-from blinkstick import blinkstick
+import time
+import random
+import board
+import busio
+import select
+import sys
+import math
 import requests
 import json
 import datetime
-import time
 import logging
 from logging.handlers import RotatingFileHandler
 import traceback
 
+from adafruit_is31fl3731.charlie_bonnet import CharlieBonnet as Display
+
+# constants and variables for the LED control
+DELAY = 0.1
+MAX_PIXELS = 16
+falling_pixels = []
+PULSE_LENGTH = 4.0
+MIN_INTENSITY = 1
+MAX_INTENSITY = 33
+
+# constants and variables for the weather checking
 WEATHER_ATTRIBUTE = "wind"
 STATION = "alpine"
 TARGET_WEATHER_ATTRIBUTE_VALUE = 2
 POLL_INTERVAL = 600  # seconds
 URL = "https://api.bridgerbowl.com/graphql"
-MAX_RETRIES = 6
+MAX_RETRIES = 10
 QUERY = """
 query Query($station: String!, $date_start: DateTime!, $date_end: DateTime!) {
   weather_readings(
@@ -49,6 +65,12 @@ HEADERS = {
     "Referrer-Policy": "strict-origin-when-cross-origin",
 }
 
+# setup for the LED display
+i2c = busio.I2C(board.SCL, board.SDA)
+display = Display(i2c)
+
+def interruptible_sleep(seconds):
+    select.select([], [], [], seconds)
 
 def check_weather_at_bridger_bowl():
     now = datetime.datetime.now()
@@ -68,9 +90,9 @@ def check_weather_at_bridger_bowl():
         logger.info("Fetching weather data, attempt " + str(attempt + 1) + " of " + str(MAX_RETRIES))
         try:
             response = requests.post(URL, json=payload)
-            response.raise_for_status()  # Raises error for unsuccessful status (i.e., not 2xx).
+            response.raise_for_status()  # raises error for unsuccessful status (i.e., not 2xx).
             weather_data = json.loads(response.text)
-            logger.info("Latest weather data: %s", json.dumps(weather_data, indent=4))
+            logger.info("Latest weather data: %s", json.dumps(weather_data["data"]["weather_readings"]["data"][0], indent=4))
             return (weather_data["data"]["weather_readings"]["data"][0][WEATHER_ATTRIBUTE] > TARGET_WEATHER_ATTRIBUTE_VALUE)
         except (requests.exceptions.RequestException, json.JSONDecodeError) as e:
             logger.error("Failed to fetch new weather data. Exception: %s", e)
@@ -80,33 +102,62 @@ def check_weather_at_bridger_bowl():
                 raise  # raise the last exception
 
 
-### Main ###
+def draw_pixels():
+    now = datetime.datetime.now()
+    end_time = (now + datetime.timedelta(hours=0, minutes=0, seconds=POLL_INTERVAL))
+
+    while datetime.datetime.now() < end_time:
+        elapsed_time = time.time() % PULSE_LENGTH  # time elapsed since the start of the current pulse
+        pulse_progress = elapsed_time / PULSE_LENGTH  # progress of the current pulse as a ratio
+        intensity = int((MAX_INTENSITY - MIN_INTENSITY) * abs(1 - 2 * pulse_progress) + MIN_INTENSITY)   
+        display.fill(intensity)
+
+        # if we're below the max pixels, add a new one at a random x-position
+        if len(falling_pixels) < MAX_PIXELS:
+            falling_pixels.append((random.randint(0, display.width - 1), 0))
+
+        # loop over a copy of the falling pixels list so we can modify it while looping
+        for pixel in falling_pixels.copy():
+            x, y = pixel
+
+            # if this pixel has hit the bottom, remove it from the list
+            if y >= display.height - 1:
+                falling_pixels.remove(pixel)
+            else:
+                # otherwise, move the pixel down and draw it
+                display.pixel(x, y, 0)  # draw pixel with zero intensity, turning it off
+                y += 1  # move pixel down
+                falling_pixels[falling_pixels.index(pixel)] = (x, y)  # update pixel position in list
+
+        interruptible_sleep(DELAY)
+
+### main ###
 try:
     logger = logging.getLogger("Rotating Log")
     logger.setLevel(logging.DEBUG)
     handler = RotatingFileHandler("/home/bbb/bbb.log", maxBytes=1000000, backupCount=5)
     logger.addHandler(handler)
 
-    led = blinkstick.find_first()
+    while True:
+        if check_weather_at_bridger_bowl():
+            logger.info("Target weather condition met!")
+            draw_pixels()
+        else:
+            logger.info("Target weather condition not met.")
+            display.fill(0)
+            time.sleep(POLL_INTERVAL)
+        logger.info("--------------------------------------")
 
-    if led is None:
-        logging.error("BlinkStick not found. Exiting.")
-    else:
-        while True:
-            if check_weather_at_bridger_bowl():
-                logger.info("Target weather condition met!")
-                # Each pulse is approximately 2s
-                led.pulse(name="blue", repeats=POLL_INTERVAL // 2)
-            else:
-                logger.info("Target weather condition not met.")
-                led.set_color(name="white")
-                time.sleep(POLL_INTERVAL)
-            logger.info("--------------------------------------")
 except KeyboardInterrupt:
     logger.info("Interrupted by user. Exiting... Bye!")
-    led.turn_off()
+    display.fill(0)
+    sys.exit(0)
+
 except Exception as e:
-    error_traceback = traceback.format_exc()  # Capture the full traceback
+    error_traceback = traceback.format_exc()  # capture the full traceback
     logger.error("Exiting with exception: %s", e)
-    logger.error("Full traceback: %s", error_traceback)  # Log the traceback
-    led.set_color(name="red")
+    logger.error("Full traceback: %s", error_traceback)
+    display.fill(0)
+    display.pixel(0, 0, 1) # turn on one pixel to indicate an error
+    sys.exit(0)
+
