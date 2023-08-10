f372571fb0c39856f48694db9d88dd5f9979117d
Perry Hooker <perry.hooker@gmail.com>
2023-08-10 13:59:21 -0600

    display wind at midway during the off-season

--- bbb.py
+++ bbb.py
@@ -14,16 +14,37 @@ import traceback
 
 from adafruit_is31fl3731.charlie_bonnet import CharlieBonnet as Display
 
+# global variables & helpers for weather checking
+WEATHER_ATTRIBUTE = None
+STATION = None
+TARGET_WEATHER_ATTRIBUTE_VALUE = None
+IS_WINTER = None
+
+def set_weather_constants():
+    global IS_WINTER, WEATHER_ATTRIBUTE, STATION, TARGET_WEATHER_ATTRIBUTE_VALUE
+
+    # Bridger's 2023 season is Dec 8 - Apr 7
+    today = datetime.date.today()
+    dec_8 = datetime.date(today.year, 12, 8)
+    apr_7 = datetime.date(today.year + 1, 4, 7)
+    IS_WINTER = (dec_8 < today < apr_7)
+
+    if IS_WINTER:
+        WEATHER_ATTRIBUTE = "new_snow"
+        STATION = "bridger"
+        TARGET_WEATHER_ATTRIBUTE_VALUE = 0
+    else:
+        WEATHER_ATTRIBUTE = "wind"
+        STATION = "midway"
+        TARGET_WEATHER_ATTRIBUTE_VALUE = 4
+
 # constants and variables for the LED control
 DELAY = 0.1
 PULSE_LENGTH = 4.0
 MIN_INTENSITY = 0
 MAX_INTENSITY = 32
 
-# constants and variables for the weather checking
-WEATHER_ATTRIBUTE = "new_snow"
-STATION = "bridger"
-TARGET_WEATHER_ATTRIBUTE_VALUE = 0
+# constants for the endpoint request
 POLL_INTERVAL = 600  # seconds
 URL = "https://api.bridgerbowl.com/graphql"
 MAX_RETRIES = 10
@@ -99,34 +120,59 @@ def check_weather_at_bridger_bowl():
             else:
                 raise  # raise the last exception
 
-def draw_pixels(max_pixels):
+def draw_snow_pixels(max_pixels):
     falling_pixels = []
     end_time = datetime.datetime.now() + datetime.timedelta(seconds=POLL_INTERVAL)
-    start_time = time.time()  # store the start time
+    start_time = time.time()
     while datetime.datetime.now() < end_time:
-        elapsed_time = (time.time() - start_time) % PULSE_LENGTH  # time elapsed since the start of the script
+        elapsed_time = (time.time() - start_time) % PULSE_LENGTH  # time elapsed since the start of the while loop
         pulse_progress = elapsed_time / PULSE_LENGTH  # progress of the current pulse as a ratio
         intensity = int((MAX_INTENSITY - MIN_INTENSITY) * abs(2 * pulse_progress - 1) + MIN_INTENSITY)
 
-        # Refresh the display
         display.fill(intensity)
 
-        # If we're below the max pixels, add a new one at a random x-position
+        # if we're below the max pixels, add a new one at a random x-position
         if len(falling_pixels) < max_pixels:
             falling_pixels.append((random.randint(0, display.width - 1), 0))
 
-        # Loop over a copy of the falling pixels list so we can modify it while looping
+        # loop over a copy of the falling pixels list so we can modify it while looping
         for pixel in falling_pixels.copy():
             x, y = pixel
 
-            # If this pixel has hit the bottom, remove it from the list
+            # if this pixel has hit the bottom, remove it from the list
             if y >= display.height - 1:
                 falling_pixels.remove(pixel)
             else:
-                # Otherwise, move the pixel down and draw it
+                # otherwise, move the pixel down and draw it
                 display.pixel(x, y, 0)  # draw pixel with zero intensity, turning it off
                 y += 1  # move pixel down
                 falling_pixels[falling_pixels.index(pixel)] = (x, y)  # update pixel position in list
+
+        interruptible_sleep(DELAY)
+
+
+def draw_wind_pixels(max_pixels):
+    falling_pixels = []
+    end_time = datetime.datetime.now() + datetime.timedelta(seconds=POLL_INTERVAL)
+    while datetime.datetime.now() < end_time:
+        display.fill(0)
+
+        # if we're below the max pixels, add a new one at a random y-position
+        if len(falling_pixels) < max_pixels:
+            falling_pixels.append((0, random.randint(0, display.height - 1)))
+
+        # loop over a copy of the falling pixels list so we can modify it while looping
+        for pixel in falling_pixels.copy():
+            x, y = pixel
+
+            # if this pixel has reached the right edge, remove it from the list
+            if x >= display.width - 1:
+                falling_pixels.remove(pixel)
+            else:
+                # otherwise, move the pixel right and draw it
+                display.pixel(x, y, MAX_INTENSITY) # turn pixel on
+                x += 1 # move pixel right
+                falling_pixels[falling_pixels.index(pixel)] = (x, y) # update pixel position in list
 
         interruptible_sleep(DELAY)
 
@@ -139,10 +185,14 @@ try:
     logger.addHandler(handler)
 
     while True:
+        set_weather_constants()
         weather_attribute_value = check_weather_at_bridger_bowl()
         if weather_attribute_value > TARGET_WEATHER_ATTRIBUTE_VALUE:
             logger.info("Target weather condition met!")
-            draw_pixels(weather_attribute_value - TARGET_WEATHER_ATTRIBUTE_VALUE)
+            if IS_WINTER:
+                draw_snow_pixels(weather_attribute_value - TARGET_WEATHER_ATTRIBUTE_VALUE)
+            else:
+                draw_wind_pixels(weather_attribute_value - TARGET_WEATHER_ATTRIBUTE_VALUE)
         else:
             logger.info("Target weather condition not met.")
             display.fill(0)
@@ -159,6 +209,6 @@ except Exception as e:
     logger.error("Exiting with exception: %s", e)
     logger.error("Full traceback: %s", error_traceback)
     display.fill(0)
-    display.pixel(0, 0, 1) # turn on one pixel to indicate an error
+    display.pixel(0, 0, MAX_INTENSITY) # turn on one pixel to indicate an error
     sys.exit(0)
 
