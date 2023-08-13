83a142f5dfa2a1c4cce285cd4844cf551bf24bd3
Perry Hooker <perry.hooker@gmail.com>
2023-08-12 20:43:44 -0600

    update startup script to check for github.com availability

--- pull_and_start_bbb.sh
+++ pull_and_start_bbb.sh
@@ -3,25 +3,15 @@
 # Define the network interface to check. For example, this could be 'eth0', 'wlan0', etc.
 INTERFACE="wlan0"
 REPO_DIR="/home/bbb/bbb/"
-
-# Function to check if the network interface is up.
-is_interface_up() {
-    local iface=$1
-    if ip link show "$iface" | grep -q "UP"; then
-        return 0
-    else
-        return 1
-    fi
-}
+LOGFILE="/home/bbb/startup_script.log"
 
 # Wait for the network interface to come up.
-while ! is_interface_up "$INTERFACE"; do
-    echo "Waiting for $INTERFACE to come up..."
+until ping -c 1 github.com; do
+    echo "Waiting for network..." >> "$LOGFILE"
     sleep 5
 done
 
-# Now that the network is up, change to the directory and execute git pull.
-cd "$REPO_DIR" && git pull
+cd "$REPO_DIR" && git pull >> "$LOGFILE" 2>&1
 # Start the Bridger Bowl Blinker
 sudo -u bbb python3 bbb.py &
 
